f76d284dbbdd1f5d5709364883bb398dd9eb6e8e
Lea Baldevieso <mlbaldevieso@gmail.com>
2024-05-24 22:49:35 +1000

    comments added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import "./App.css";
-import Homepage from "./pages/Homepage";
 import { useMediaQuery } from "react-responsive";
 import { useState, useEffect } from "react";
+import Homepage from "./pages/Homepage";
 import Landing from "./pages/Landing";
 
+// Set a delay of 2 seconds (2000 milliseconds)
 const delay = 2 * 1000;
 
 function App() {
+  // Define a state variable for loading status, initialized to true
   const [isLoading, setLoading] = useState(true);
+  // Define a media query for mobile or tablet devices with a max-width of 991px
   const isMobileOrTablet = useMediaQuery({ query: "(max-width: 991px)" });
 
+  // useEffect hook to set a timer that updates the loading status after the delay
   useEffect(() => {
     let timer = setTimeout(() => {
+      // Set loading to false after the delay
       setLoading(false);
     }, delay);
 
+    // Clean up the timer on component unmount to avoid memory leaks
     return () => {
       clearTimeout(timer);
     };
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,74 +1,98 @@
-import { useState, useEffect } from "react";
-
 import { People } from "../utils/people_api.js";
-import MobileHomepage from "../components/MobileHomepage.jsx";
-import WebHomepage from "../components/WebHomepage.jsx";
-import SortPeople from "../helpers/SortPeople.js";
+import { useState, useEffect } from "react";
 import Markers from "../helpers/Markers.js";
+import MobileHomepage from "../components/MobileHomepage.jsx";
+import SortPeople from "../helpers/SortPeople.js";
+import WebHomepage from "../components/WebHomepage.jsx";
 
 export default function Homepage({ isMobileOrTablet }) {
+  // State to hold the list of people
   const [people, setPeople] = useState([]);
+  // State for the search input value
   const [searchInput, setSearchInput] = useState("");
+  // State for the selected person
   const [selectedPerson, setSelectedPerson] = useState({});
+  // State for sidebar visibility
   const [isSidebarVisible, setSidebarVisible] = useState(true);
+  // State for main panel visibility, initially set based on device type
   const [isMainPanelVisible, setMainPanelVisible] = useState(!isMobileOrTablet);
+  // State for more details visibility
   const [isMoreDetailsVisible, setMoreDetailsVisible] = useState(false);
+  // State for the details button text
   const [detailsButtonText, setDetailsButtonText] = useState("show more");
+  // State for person item visibility, initially set based on device type
   const [isPersonItemVisible, setPersonItemVisible] =
     useState(isMobileOrTablet);
 
+  // Function to fetch people data from the API
   const peopleData = async () => {
     try {
+      // Fetch data from API
       const peopleData = await People.all();
+      // Sort and set people data
       setPeople(Markers(SortPeople(peopleData)));
     } catch (error) {
       console.error(error);
     }
   };
 
+  // useEffect hook to fetch people data when the component mounts
   useEffect(() => {
     peopleData();
   }, []);
 
+  // Filter the people data based on the search input
   const filteredPeople = Object.keys(people).reduce((acc, initial) => {
     const filtered = people[initial].filter((person) =>
       person.name.toLowerCase().includes(searchInput.toLowerCase())
     );
     if (filtered.length > 0) {
+      // Add filtered results to accumulator
       acc[initial] = filtered;
     }
 
     return acc;
   }, {});
 
+  // Handle selecting a person from people list
   const handleSelectPerson = (person) => {
+    // Set the selected person
     setSelectedPerson(person);
     if (isMobileOrTablet) {
+      // Hide sidebar on mobile/tablet
       setSidebarVisible(false);
+      // Show person item on mobile/tablet
       setPersonItemVisible(true);
     }
+    // Show main panel
     setMainPanelVisible(true);
+    // Show person item
     setPersonItemVisible(true);
   };
 
+  // Handle the back button click from main panel
   const handleBackClick = () => {
     if (isMobileOrTablet) {
+      // Hide main panel on mobile/tablet
       setMainPanelVisible(false);
+      // Show side bar on mobile/tablet
       setSidebarVisible(true);
     } else {
+      // Hide person item on desktop
       setPersonItemVisible(false);
     }
+    // Hide more details
     setMoreDetailsVisible(false);
   };
 
+  // Handle the show more details button click
   const handleShowMoreDetails = () => {
-    isMoreDetailsVisible
-      ? setMoreDetailsVisible(false)
-      : setMoreDetailsVisible(true);
-
-    detailsButtonText === "show more"
-      ? setDetailsButtonText("show less")
-      : setDetailsButtonText("show more");
+    // Toggle more details visibility
+    setMoreDetailsVisible((prev) => !prev);
+    // Toggle button text
+    setDetailsButtonText((prev) =>
+      prev === "show more" ? "show less" : "show more"
+    );
   };
 
   return (
