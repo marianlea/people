30ac8323c0a6f3b361bde53cc389024b47216e38
Lea Baldevieso <mlbaldevieso@gmail.com>
2024-05-24 19:19:58 +1000

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: "/vercel/path0/src/utils/people_api.js",
+    },
+  },
+});
